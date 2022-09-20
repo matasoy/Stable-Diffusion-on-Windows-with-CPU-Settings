23a9cdc1ef76da85b00c294835884ec31949b3a6
matasoy <matasoy@gmail.com>
2022-09-21 02:22:48 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,38 @@
+import tkinter as tk
+import customtkinter as ctk 
+
+from PIL import ImageTk
+access_token  = "WRITE_YOUR_TOKEN_HERE" #this will be your token from https://huggingface.co/settings/tokens, its free
+
+import torch
+from torch import autocast
+from diffusers import StableDiffusionPipeline 
+
+# Create the app
+app = tk.Tk()
+app.geometry("532x632")
+app.title("Stable Bud") 
+ctk.set_appearance_mode("dark") 
+
+prompt = ctk.CTkEntry(height=40, width=512, text_font=("Arial", 20), text_color="black", fg_color="white") 
+prompt.place(x=10, y=10)
+
+lmain = ctk.CTkLabel(height=512, width=512)
+lmain.place(x=10, y=110)
+
+modelid = "CompVis/stable-diffusion-v1-4"
+device = "cpu"
+pipe = StableDiffusionPipeline.from_pretrained(modelid, use_auth_token=access_token, torch_dtype=torch.float32) 
+
+def generate(): 
+    image = pipe(prompt.get(), guidance_scale=8.5, generator=torch.Generator(device=device))["sample"][0]
+    
+    image.save('generatedimage.png')
+    img = ImageTk.PhotoImage(image)
+    lmain.configure(image=img) 
+
+trigger = ctk.CTkButton(height=40, width=120, text_font=("Arial", 20), text_color="white", fg_color="blue", command=generate) 
+trigger.configure(text="Generate") 
+trigger.place(x=206, y=60) 
+
+app.mainloop()
